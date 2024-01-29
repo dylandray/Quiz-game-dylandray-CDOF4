1332588772935b3cea229770fa8d4bc369570167
dylandray <120407558+dylandray@users.noreply.github.com>
2024-01-29 15:18:11 +0100

    Update grandquiz.py
    Ajout du choix de la difficulté
    Ajout du choix du nombre de questions

--- grandquiz.py
+++ grandquiz.py
@@ -3,31 +3,93 @@
 
 import random
 
-Questions = [("Quel est le nom de la capitale de la France ?", "Paris"),("Quel est le nom de la capitale de l'Espagne ?", "Madrid"),("Quel est le nom de la capitale de l'Italie ?", "Rome"),("Quel est le nom de la capitale de l'Allemagne ?", "Berlin"),("Quel est le nom de la capitale de la Belgique ?", "Bruxelles"),("Quel est le nom de la capitale de la Suisse ?", "Berne"),("Quel est le nom de la capitale de la Russie ?", "Moscou"),("Quel est le nom de la capitale de la Chine ?", "Pekin"),("Quel est le nom de la capitale de l'Inde ?", "New Delhi"),("Quel est le nom de la capitale de l'Egypte ?", "Le Caire")]
 
-points=0
+def GrandQuiz():
+    QuestionsFacile = [("Quel est le nom de la capitale de la France ?", "Paris"),("Quel est le nom de la capitale de l'Espagne ?", "Madrid"),("Quel est le nom de la capitale de l'Italie ?", "Rome"),("Quel est le nom de la capitale de l'Allemagne ?", "Berlin"),("Quel est le nom de la capitale de la Belgique ?", "Bruxelles"),("Quel est le nom de la capitale de la Suisse ?", "Berne"),("Quel est le nom de la capitale de la Russie ?", "Moscou"),("Quel est le nom de la capitale de la Chine ?", "Pekin"),("Quel est le nom de la capitale de l'Inde ?", "New Delhi"),("Quel est le nom de la capitale de l'Egypte ?", "Le Caire")]
 
-while len(Questions) != 0:
-    x = random.randint(0,len(Questions)-1)
-    print(Questions[x][0])
-    reponse = input("Votre réponse : ")
-    if reponse == Questions[x][1]:
-        points +=1
-        print("Bonne réponse")
-        del Questions[x]
-    else: 
-        print("Mauvaise réponse!")
-        del Questions[x]
+    QuestionsMoyen = [
+        ("Quelle est la capitale de l'Espagne ?", "Madrid"),
+        ("Quelle est la capitale de l'Italie ?", "Rome"),
+        ("Quelle est la capitale du Canada ?", "Ottawa"),
+        ("Quelle est la capitale du Japon ?", "Tokyo"),
+        ("Quelle est la capitale de l'Australie ?", "Canberra"),
+        ("Quelle est la capitale de l'Allemagne ?", "Berlin"),
+        ("Quelle est la capitale de la France ?", "Paris"),
+        ("Quelle est la capitale de la Chine ?", "Pékin"),
+        ("Quelle est la capitale du Brésil ?", "Brasília"),
+        ("Quelle est la capitale des États-Unis ?", "Washington, D.C.")
+    ]
 
-print("Vous avez eu "+str(points)+" bonnes réponses!")
-if points < 3:
-    print("Vous devriez réviser votre géographie..")
+    QuestionsDures = [
+        ("Quelle est la capitale du Bangladesh ?", "Dacca"),
+        ("Quelle est la capitale de l'Oman ?", "Mascate"),
+        ("Quelle est la capitale du Mozambique ?", "Maputo"),
+        ("Quelle est la capitale de la Croatie ?", "Zagreb"),
+        ("Quelle est la capitale du Guatemala ?", "Guatemala"),
+        ("Quelle est la capitale de la Lettonie ?", "Riga"),
+        ("Quelle est la capitale de la Bolivie ?", "La Paz"),
+        ("Quelle est la capitale de la Slovénie ?", "Ljubljana"),
+        ("Quelle est la capitale du Liban ?", "Beyrouth"),
+        ("Quelle est la capitale du Paraguay ?", "Asunción")
+    ]
+
+    QuestionsImpo = [
+        ("Quelle est la capitale du Bhoutan ?", "Thimphou"),
+        ("Quelle est la capitale du Lesotho ?", "Maseru"),
+        ("Quelle est la capitale du Tadjikistan ?", "Douchanbé"),
+        ("Quelle est la capitale de la Mongolie ?", "Oulan-Bator"),
+        ("Quelle est la capitale du Suriname ?", "Paramaribo"),
+        ("Quelle est la capitale de la Sierra Leone ?", "Freetown"),
+        ("Quelle est la capitale du Kirghizistan ?", "Bichkek"),
+        ("Quelle est la capitale de la Mauritanie ?", "Nouakchott"),
+        ("Quelle est la capitale du Malawi ?", "Lilongwe"),
+        ("Quelle est la capitale du Nauru ?", "Yaren")
+    ]
+
+    points=0
+    compteur=0
+    nbquestions=0
+    difficulte = 0
     
-elif points < 7:
-    print("Continuez vos efforts!")
-    
-elif points <= 9:
-    print("Bon boulot !!")
+    print("C'est parti ! Choississez la difficulté en tapant le nombre associé:"
+          "\n1. Facile"
+          "\n2. Moyen"
+          "\n3. Difficile"
+          "\n4. Impossible"
+          "\n5. Tout mélangé!")
+    while difficulte <= 0 or difficulte > 5:
+        print("Appuyez sur 1,2,3,4 ou 5")
+        difficulte = int(input("Votre choix: "))
+    if difficulte == 1:
+        Questions = QuestionsFacile
+    elif difficulte == 2:
+        Questions = QuestionsMoyen
+    elif difficulte == 3:
+        Questions = QuestionsDures
+    elif difficulte == 4:
+        Questions = QuestionsImpo
+    elif difficulte == 5:
+        Questions = QuestionsFacile + QuestionsMoyen + QuestionsDures + QuestionsImpo
 
-elif points == 10:
-    print("WOOOOOW Bravo!!! Perfect")
+    print("Combien de questions voulez-vous ? (10 maximum)")
+    nbquestions = int(input("Votre choix : "))
+    while nbquestions > 10 or nbquestions < 1:
+        print("Le nombre de questions doit être compris entre 1 et 10")
+        nbquestions = int(input("Votre choix : "))
+
+    print(nbquestions)
+    while compteur < nbquestions:
+        compteur+=1
+        x = random.randint(0,len(Questions)-1)
+
+        print(Questions[x][0])
+        reponse = input("Votre réponse : ")
+        if reponse == Questions[x][1]:
+            points +=1
+            print("Bonne réponse")
+            del Questions[x]
+        else: 
+            print("Mauvaise réponse!")
+            del Questions[x]
+
+    print("Vous avez eu "+str(points)+" bonnes réponses!")
